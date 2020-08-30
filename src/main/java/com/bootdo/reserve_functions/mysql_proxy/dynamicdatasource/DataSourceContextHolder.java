7969e62f3339a38ba484dff23fb7d973f867377b
wu <1428944541@qq.com>
2020-08-30 21:01:19 +0800

    MySQL注解，读写分离数据库

---
+++ src/main/java/com/bootdo/reserve_functions/mysql_proxy/dynamicdatasource/DataSourceContextHolder.java
@@ -0,0 +1,21 @@
+package com.bootdo.reserve_functions.mysql_proxy.dynamicdatasource;
+
+/**
+ * @author luoping
+ */
+public class DataSourceContextHolder {
+	private static final ThreadLocal<String> DYNAMIC_DATASOURCE_CONTEXT = new ThreadLocal<>();
+
+	public static void set(String datasourceType) {
+		DYNAMIC_DATASOURCE_CONTEXT.set(datasourceType);
+	}
+
+	public static String get() {
+		return DYNAMIC_DATASOURCE_CONTEXT.get();
+	}
+
+	public static void clear() {
+		DYNAMIC_DATASOURCE_CONTEXT.remove();
+	}
+
+}
