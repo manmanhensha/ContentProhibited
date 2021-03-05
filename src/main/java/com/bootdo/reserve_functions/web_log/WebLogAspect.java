b002a7edb0adbf86535d83cf1720265e7673fb7e
wushiqiang <1428944541@qq.com>
2021-03-05 20:56:40 +0800

    日志切面更新，把可能产生异常的都catch掉

--- src/main/java/com/bootdo/reserve_functions/web_log/WebLogAspect.java
+++ src/main/java/com/bootdo/reserve_functions/web_log/WebLogAspect.java
@@ -1,8 +1,5 @@
 package com.bootdo.reserve_functions.web_log;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import org.aspectj.lang.JoinPoint;
@@ -16,17 +13,13 @@ import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @Order(1)
 @Aspect
@@ -48,40 +41,6 @@ public class WebLogAspect {
 		}
 	};
 
-	private static String getParamValue(JoinPoint joinPoint) {
-		StringBuilder sb = new StringBuilder();
-		//获取所有的参数
-		try {
-			Object[] args = joinPoint.getArgs();
-			for (Object arg : args) {
-				//          2 通过反射获取实体类属性
-				sb.append(getFieldsValue(arg));
-			}
-		} catch (Exception e) {
-			return "";
-		}
-		return String.valueOf(sb);
-	}
-
-	private static String getFieldsValue(Object obj) {
-		//通过反射获取所有的字段，getFileds()获取public的修饰的字段
-		StringBuilder sb = new StringBuilder();
-		sb.append("{");
-		try {
-			Field[] fields = obj.getClass().getDeclaredFields();
-			for (Field f : fields) {
-				sb.append("\"").append(f.getName()).append("\"").append(":");
-				if (ObjectUtil.isNotNull(f.get(obj))) {
-					sb.append("\"").append(f.get(obj)).append("\"").append(",");
-				} else {
-					sb.append(f.get(obj)).append(",");
-				}
-			}
-		} catch (Exception e) {
-			log.info("获取字段为空");
-		}
-		return sb.toString();
-	}
 
 	//返回方法的参数名
 	private static String[] getFieldsName(JoinPoint joinPoint) {
@@ -112,7 +71,7 @@ public class WebLogAspect {
 		}
 	}
 
-	@Pointcut("execution(* com.faw.jetta.manage.controller..*.*(..))")
+	@Pointcut("execution(* com.faw.jetta.controller..*.*(..))")
 	public void logPointCut() {
 	}
 
@@ -130,23 +89,8 @@ public class WebLogAspect {
 		log.info("HTTP 请求方式 : " + request.getMethod());
 		log.info("CLASS 方法 : " + joinPoint.getSignature().getDeclaringTypeName() + "."
 				+ joinPoint.getSignature().getName());
-		String paramValue = getParamValue(joinPoint);
-		if (StrUtil.isNotBlank(paramValue) && !"{".equals(paramValue)) {
-			try {
-				String[] realFieldsName = getFieldsName(joinPoint);
-				String s = StrUtil.subWithLength(paramValue, 0, paramValue.length() - 1) + "}";
-				Map<String, Object> objectMap = BeanUtil.beanToMap(JSONUtil.parseObj(s), false, true);
-				StringBuilder fieldsName = new StringBuilder();
-				for (Class<?> parameterType : method.getParameterTypes()) {
-					fieldsName.append(parameterType.getName());
-				}
-				log.info("请求参数: " + fieldsName + ": " + Arrays.toString(realFieldsName) + "  " + JSONUtil.toJsonStr(objectMap));
-			} catch (Exception e) {
-				log.info("请求参数: " + paramValue);
-			}
-		} else {
-			log.info("请求参数为空");
-		}
+		String[] realFieldsName = getFieldsName(joinPoint);
+		log.info("请求参数 : " + Arrays.toString(realFieldsName) + "  " + JSONUtil.toJsonStr(joinPoint.getArgs()));
 	}
 
 	/**
@@ -167,39 +111,4 @@ public class WebLogAspect {
 		return ob;
 	}
 
-	/**
-	 * 根据方法和传入的参数获取请求参数,依靠RequestBody和RequestParam注解实现
-	 */
-	private Object getParameter(Method method, Object[] args) {
-		List<Object> argList = new ArrayList<>();
-		Parameter[] parameters = method.getParameters();
-		for (int i = 0; i < parameters.length; i++) {
-			//将RequestBody注解修饰的参数作为请求参数
-			RequestBody requestBody = parameters[i].getAnnotation(RequestBody.class);
-			if (requestBody != null) {
-				argList.add(args[i]);
-			}
-			//将RequestParam注解修饰的参数作为请求参数
-			RequestParam requestParam = parameters[i].getAnnotation(RequestParam.class);
-			if (requestParam != null) {
-				Map<String, Object> map = new HashMap<>();
-				String key = parameters[i].getName();
-				if (!StringUtils.isEmpty(requestParam.value())) {
-					key = requestParam.value();
-				}
-				map.put(key, args[i]);
-				argList.add(map);
-			}
-			if (requestBody == null && requestParam == null) {
-				argList.add(parameters[i].toString());
-			}
-		}
-		if (argList.size() == 0) {
-			return null;
-		} else if (argList.size() == 1) {
-			return argList.get(0);
-		} else {
-			return argList;
-		}
-	}
 }
